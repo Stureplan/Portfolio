6061c3f6d8f31b0149c81b56483d208d768a63b3
Stureplan <linus.ax@gmail.com>
2023-08-31 20:14:48 +0200

    Compression, should reduce page load

--- hover.js
+++ hover.js
@@ -1,21 +1,3 @@
-/*const videos = document.querySelectorAll('.video');
-videos.forEach(video => {
-  video.addEventListener('mouseenter', () => {
-    video.muted = true;
-    video.play();
-  });
-  
-  video.addEventListener('mouseleave', () => {
-    video.muted = true;
-    video.pause();
-  });
-
-  video.addEventListener('click', () => {
-
-    window.location.href = "generic.html";
-  });
-});*/
-
 function hoverEnter(video)
 {
     video.muted = true;
@@ -26,9 +8,4 @@ function hoverExit(video)
 {
     video.muted = true;
     video.pause();
-}
-
-function clickVideo(destination)
-{
-    window.location.href = destination;
 }
